7177062d23bfbfcc4c0203bea4816fb98ebf7c9a
Suhail Hasan Kg <official.aitra.tech@gmail.com>
2023-11-06 17:37:33 +0530

    Successful session. We can get all the files from the dir

--- Canopus-File Structure Recorder/CLI Based/testmain.py
+++ Canopus-File Structure Recorder/CLI Based/testmain.py
@@ -9,4 +9,20 @@
 - Understand how the file system in python works
 - Record every file structure in a list, then pickle the list
 - When loaded any recorded structure, unpickle the data into a list
+
+* I need to store each and everyfile with it's path stored along with the file name under the root given
+* Ex- C:\\Users\\Suhail\\Desktop\\Project\\main.py [root is "C:\\"]
 """
+import os
+
+def get_files(directory):
+    all_files = []
+    
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            all_files.append(filepath)
+
+    return all_files
+
+print(get_files("C:\\Users\\Suhail Hasan Kg\\Desktop"))
