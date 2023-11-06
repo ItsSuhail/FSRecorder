659f74f8c803149384b666e126362fc802a7e1a9
Suhail Hasan Kg <official.aitra.tech@gmail.com>
2023-11-06 17:17:10 +0530

    testing things

---
+++ Canopus-File Structure Recorder/CLI Based/testmain.py
@@ -0,0 +1,12 @@
+"""
+# My Idea: 
++ We input the required directory to be recorded
++ Each element of the directry will be recorded
++ we'll save the recorded file and serialize it with pickle
++ we can see the differences between 2 recordings i.e. we can see the changes which occured
+
+
+- Understand how the file system in python works
+- Record every file structure in a list, then pickle the list
+- When loaded any recorded structure, unpickle the data into a list
+"""
