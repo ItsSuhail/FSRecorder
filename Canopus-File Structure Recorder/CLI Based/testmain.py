e58b9c03d0050bb172ac0b5930949669e97eeaf7
Suhail Hasan Kg <official.aitra.tech@gmail.com>
2023-11-06 19:07:10 +0530

    Test successful for classifying the filenames by their roots

--- Canopus-File Structure Recorder/CLI Based/testmain.py
+++ Canopus-File Structure Recorder/CLI Based/testmain.py
@@ -11,18 +11,35 @@
 - When loaded any recorded structure, unpickle the data into a list
 
 * I need to store each and everyfile with it's path stored along with the file name under the root given
-* Ex- C:\\Users\\Suhail\\Desktop\\Project\\main.py [root is "C:\\"]
+* Ex- C:\\Users\\Suhail\\Desktop\\Project\\main.py [root is "C:\\Users\\Suhail\\Desktop\\Project"]
 """
 import os
 
 def get_files(directory):
     all_files = []
+    all_roots = []
     
     for root, dirs, files in os.walk(directory):
         for filename in files:
+            # print(root)
             filepath = os.path.join(root, filename)
             all_files.append(filepath)
+            all_roots.append(root)
 
-    return all_files
+    return all_files, all_roots
 
-print(get_files("C:\\Users\\Suhail Hasan Kg\\Desktop"))
+x,y = get_files("C:\\Users\\Suhail Hasan Kg\\Desktop")
+
+def classify(ar1, ar2):
+    finalDict = {}
+    n = 0
+    for e in ar2:
+        try:
+            data = finalDict[e]
+            finalDict.update({e:(data + ":|:" + ar1[n])})
+        except KeyError:
+            finalDict.update({e:(ar1[n])})
+        n+=1
+    
+    return finalDict
+
